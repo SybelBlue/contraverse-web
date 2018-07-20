d9cb8239cb31dc5998b1f8496c027fa714fa7a99
Keara Berlin <kearaberlin@gmail.com>
2018-07-20 16:37:09 -0700

    made / redirect to /login for now (I couldn't get it to redirect to /home without throwing an error)

--- contra/lib/contra_web/controllers/page_controller.ex
+++ contra/lib/contra_web/controllers/page_controller.ex
@@ -2,6 +2,9 @@ defmodule ContraWeb.PageController do
   use ContraWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    # TODO if user is not logged in, redirect to login.
+    # for now, redirecting to /login always
+    redirect conn, to: "/login"
+    # render conn, "index.html"
   end
 end
