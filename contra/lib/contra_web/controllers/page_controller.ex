01afe140212c0bf7fac0351ecaa65cb8257a6631
Keara Berlin <kearaberlin@gmail.com>
2018-07-21 19:04:28 -0700

    removed redirect to login

--- contra/lib/contra_web/controllers/page_controller.ex
+++ contra/lib/contra_web/controllers/page_controller.ex
@@ -2,9 +2,6 @@ defmodule ContraWeb.PageController do
   use ContraWeb, :controller
 
   def index(conn, _params) do
-    # TODO if user is not logged in, redirect to login.
-    # for now, redirecting to /login always
-    redirect conn, to: "/login"
-    # render conn, "index.html"
+    render conn, "index.html"
   end
 end
