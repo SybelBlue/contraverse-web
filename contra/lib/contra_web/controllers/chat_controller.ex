c382294a7cd5270bc571a9c54ca083edc0f5e53c
SybelBlue <lbc.dna@gmail.com>
2018-07-27 09:39:08 -0700

    Style changes

--- contra/lib/contra_web/controllers/chat_controller.ex
+++ contra/lib/contra_web/controllers/chat_controller.ex
@@ -12,15 +12,15 @@ defmodule ContraWeb.ChatController do
     open = (chat == :open)
 
     messages = [
-      %{:sent => true, :text => "Hello, how are you?"},
-      %{:sent => false, :text => "I'm good, thanks for asking. How are you doing?"},
-      %{:sent => true, :text => "I am great. So can I ask what your reasoning was behind your answer to the prompt?"},
-      %{:sent => false, :text => "I was about to ask you the same thing! Great minds truly do think alike."}
+      %{sent: true, text: "Hello, how are you?"},
+      %{sent: false, text: "I'm good, thanks for asking. How are you doing?"},
+      %{sent: true, text: "I am great. So can I ask what your reasoning was behind your answer to the prompt?"},
+      %{sent: false, text: "I was about to ask you the same thing! Great minds truly do think alike."}
     ]
     responses = %{
-      :question => "Here is where the question will go.",
-      :yours => "Yes.",
-      :theirs => "No."
+      question: "Here is where the question will go.",
+      yours: "Yes.",
+      theirs: "No."
     }
     # assign and render
     conn
