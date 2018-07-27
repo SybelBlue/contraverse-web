afd3419e8c32cc742fb9f1e0a4bec58d1fc8526a
SybelBlue <lbc.dna@gmail.com>
2018-07-27 09:13:06 -0700

    Minor naming changes

--- contra/assets/js/specific_questions.js
+++ contra/assets/js/specific_questions.js
@@ -5,7 +5,7 @@ let SpecificQuestions = {
     let channel = this.join(socket)
 
     // load questions here
-    let area = document.getElementById("question-area")
+    let area = document.getElementById("continue-link")
 
     // matchmaking and chat_id generation here
     let id = "temp_id" //fix
@@ -15,10 +15,14 @@ let SpecificQuestions = {
     return channel;
   },
 
-  join(socket){
+  join(socket) {
       let channelStr = "convo-setup:specific-questions"
       let channel = CommonFunctions.joinChannel(socket, channelStr, {})
       return channel
+  },
+
+  onContinueToChat() {
+
   },
 }
 
